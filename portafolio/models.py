5d58b650a8efaafbb64465f20d7f9d3bc916ae86
FreddyxD5 <freddyxd5@gmail.com>
2022-12-07 19:34:02 -0500

    añadido tema con css y js

--- portafolio/models.py
+++ portafolio/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Portafolio(models.Model):
     image_url = models.URLField('IMAGE URL del proyecto')
-    title = models.CharField()
-    description = models.CharField()
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=200)
     tags = models.CharField(max_length=200)
     repo_url = models.URLField('Repositorio GIT_URL')
     
