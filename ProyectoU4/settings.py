b9ed1708f052faeca62de65834dfb59d3f94f46a
FreddyxD5 <freddyxd5@gmail.com>
2022-12-10 12:37:56 -0500

    smt

--- ProyectoU4/settings.py
+++ ProyectoU4/settings.py
@@ -127,9 +127,8 @@ CSRF_TRUSTED_ORIGINS = ['https://proyectou4-production.up.railway.app']
 
 STATIC_URL = 'static/'
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    BASE_DIR / 'staticfiles'
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
